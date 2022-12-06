75b0ee23c96b2a76531ba58a99e16515b75034a1
ThanhLe <nhatthanh5891@gmail.com>
2022-12-06 08:08:09 +0700

    update code

--- src/modules/sms/controllers/sms.controller.ts
+++ src/modules/sms/controllers/sms.controller.ts
@@ -15,7 +15,7 @@ import {
   Logger,
   Param,
   Post,
-  Req,
+  Query,
 } from '@nestjs/common';
 import {
   ApiOkResponse,
@@ -28,7 +28,6 @@ import { CallbackDTO } from '../dto/callback.dto';
 import { CreateDTO } from '../dto/create.dto';
 import { CreateSmsParams } from '../dto/create.param';
 import { SmsService } from '../services';
-import { Request } from 'express';
 
 @Controller('sms')
 @ApiTags('sms')
@@ -40,12 +39,85 @@ export class SmsController {
     private readonly userService: UserService,
   ) {}
 
-  @Post('sms-callback')
-  async callbacksms(@Body() body: CallbackDTO) {
-    this.logger.log(body.mobile, 'sms-callback');
+  // @Post('sms-callback')
+  // async callbacksms(@Body() body: CallbackDTO) {
+  //   this.logger.log(body.mobile, 'sms-callback');
+  //   try {
+  //     const smsEntity = await this.smsService.findById(
+  //       body.info.trim() as unknown as number,
+  //     );
+  //     if (!smsEntity) {
+  //       return '0|Yêu cầu không tồn tại.';
+  //     }
+
+  //     if (!smsEntity.validTime()) {
+  //       return '0|Thời gian hiệu lực hết hạn.';
+  //     }
+
+  //     const userEntitys = await this.userService.findByUserName(
+  //       smsEntity.userName,
+  //     );
+
+  //     const userEntity = userEntitys[0];
+
+  //     if (userEntity.phone !== body.mobile) {
+  //       return '0|Số điện thoại không khớp trong tài khoản.';
+  //     }
+
+  //     let userUpdate: IUpdateUserDTO = {};
+  //     let msg = '';
+  //     switch (smsEntity.action) {
+  //       case 'phonechange':
+  //         msg = 'Đổi số điện thoại';
+  //         userUpdate = {
+  //           phone: smsEntity.info1,
+  //         };
+  //         break;
+  //       case 'passwordchange':
+  //         msg = 'Đổi mật khẩu game';
+  //         userUpdate = {
+  //           passWord: smsEntity.info1,
+  //         };
+  //         break;
+  //       case 'secpasschange':
+  //         msg = 'Đổi mật khẩu cấp 2';
+  //         userUpdate = {
+  //           passWordSecond: smsEntity.info1,
+  //         };
+  //         break;
+  //       case 'secretquestionchange':
+  //         msg = 'Đổi câu hỏi bí mật và trả lời';
+  //         userUpdate = {
+  //           question: smsEntity.info1,
+  //           answer: smsEntity.info2,
+  //         };
+  //       case 'unlockequipment':
+  //         msg = 'Mở khoá trang bị';
+  //         userUpdate = {
+  //           point: 1,
+  //         };
+  //         break;
+  //       default:
+  //         break;
+  //     }
+
+  //     await this.userService.update(smsEntity.userName, userUpdate);
+  //     this.smsService.delete(smsEntity.id);
+  //     return `0|${msg} thành công. Cảm ơn bạn đã sử dụng dịch vụ.`;
+  //   } catch (e: unknown) {
+  //     const errors = e as Error;
+  //     this.logger.error(errors.message, errors.name);
+  //     return '0|Có lỗi trong quá trình xử lý, vui lòng liên hệ gm.';
+  //   }
+  // }
+
+  @Get('sms-callback')
+  async getcallbacksms(@Query() qu: CallbackDTO) {
+    // this.logger.log(request.method, request.body, request.hostname);
+    this.logger.log(qu.mobile, 'sms-callback');
     try {
       const smsEntity = await this.smsService.findById(
-        body.info.trim() as unknown as number,
+        qu.info.trim() as unknown as number,
       );
       if (!smsEntity) {
         return '0|Yêu cầu không tồn tại.';
@@ -61,7 +133,7 @@ export class SmsController {
 
       const userEntity = userEntitys[0];
 
-      if (userEntity.phone !== body.mobile) {
+      if (userEntity.phone !== qu.mobile) {
         return '0|Số điện thoại không khớp trong tài khoản.';
       }
 
@@ -110,12 +182,6 @@ export class SmsController {
       this.logger.error(errors.message, errors.name);
       return '0|Có lỗi trong quá trình xử lý, vui lòng liên hệ gm.';
     }
-  }
-
-  @Get('sms-callback')
-  async getcallbacksms(@Req() request: Request) {
-    this.logger.log(request.method, request.body, request.hostname);
-    return '0|Có lỗi trong quá trình xử lý, vui lòng liên hệ gm.';
   }
 
   @JwtAuth()
