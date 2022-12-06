bf18064858cb7b8d3590bfa91af6100e9c7b17de
ThanhLe <nhatthanh5891@gmail.com>
2022-12-06 11:19:05 +0700

    udpate code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "jxweb-api",
-    script: "yarn start:prod",
+    script: "npm run start:prod",
     env_production: {
       NODE_ENV: "production",
     }
